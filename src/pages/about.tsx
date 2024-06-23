28bf2a4a9363f0c1f3b294a94cf020d34010cd72
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2024-06-23 17:45:10 +0200

    add img

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,6 +5,7 @@ const AboutPage = () => {
     return (
         <Layout pageTitle="About Me">
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+            <img src={"https://images.pexels.com/photos/237272/pexels-photo-237272.jpeg?auto=compress&cs=tinysrgb&w=600"} alt={'sea'}/>
         </Layout>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
-import * as React from 'react';
-import Layout from '../components/layout';
+import * as React from 'react'
+import Layout from '../components/layout'
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>I'm making this by following the Gatsby Tutorial.</p>
+            <img src={"https://images.pexels.com/photos/1450360/pexels-photo-1450360.jpeg?auto=compress&cs=tinysrgb&w=600"} alt={'sea'}/>
         </Layout>
     )
 }
 
 export const Head = () => <title>Home Page</title>
 
-export default IndexPage;
+export default IndexPage
